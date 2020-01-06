c87e2e8470af2a871f55e11c48b489c912420520
Anuj Parikh <parikh_anuj@cat.com>
2020-01-06 14:14:42 -0600

    enable theme for ant design system

--- config-overrides.js
+++ config-overrides.js
@@ -1,12 +1,15 @@
 const { override, fixBabelImports, addLessLoader } = require('customize-cra');
+const aliyunTheme = require('@ant-design/aliyun-theme');
+console.log(aliyunTheme);
 
 module.exports = override(
-    fixBabelImports('import', {
+  fixBabelImports('import', {
     libraryName: 'antd',
     libraryDirectory: 'es',
     style: true,
-    }),
-    addLessLoader({
-        javascriptEnabled: true,
-    }),
+  }),
+  addLessLoader({
+    javascriptEnabled: true,
+    modifyVars: aliyunTheme.default,
+  }),
 );
