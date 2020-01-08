e4712c2e26cc572e08b5c8a8ee6c601fd149bac8
Anuj Parikh <parikh_anuj@cat.com>
2020-01-07 22:29:52 -0600

    reusable Logo component to be used for different pages

---
+++ src/components/Logo/Logo.component.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Logo.component.less';
+
+const Logo: React.FC = () => {
+  return <img className="logo" src="/logo-blue.png" alt="Logo" />;
+};
+
+export default Logo;
