94daf393ae16c3a12e60793aa8de7e8f3eeae36c
Anuj Parikh <parikh_anuj@cat.com>
2020-01-14 22:29:19 -0600

    style changes to Header component

--- src/components/Header/Header.component.tsx
+++ src/components/Header/Header.component.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { PageHeader, Button, Badge } from 'antd';
 import './Header.component.less';
 
+//TODO: Use reusable TAG component inplace of button
 const Header: React.FC = () => {
   return (
     <PageHeader
@@ -9,7 +10,7 @@ const Header: React.FC = () => {
       ghost={false}
       title="Dashboard"
       extra={[
-        <Badge key="1" dot={true} offset={[-5, 5]}>
+        <Badge key="1" dot={true} offset={[-3, 3]}>
           <Button className="header-icons" shape="round" size="small">
             Pending Feedback
           </Button>
