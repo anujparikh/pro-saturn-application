96dd1738720ed39a46abb1e226d634b914cfa1ce
Anuj Parikh <parikh_anuj@cat.com>
2020-04-01 00:13:16 -0500

    setup redux state for interview entity

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,5 +1,10 @@
 import { questionsReducer } from '../services/Question/reducer';
 import { templatesReducer } from '../services/Template/reducer';
+import { interviewsReducer } from '../services/Interview/reducer';
 import { combineReducers } from 'redux';
 
-export default combineReducers({ questions: questionsReducer, templates: templatesReducer });
+export default combineReducers({
+  questions: questionsReducer,
+  templates: templatesReducer,
+  interviews: interviewsReducer,
+});
